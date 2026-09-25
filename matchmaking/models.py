73daca25d6815eece06b08cfe0945a9ad76905b6
kacper kubiak <kacperkubiak.magik@wp.pl>
2026-09-25 18:23:43 +0200

    feat: Add MatchmakingEntry model

--- matchmaking/models.py
+++ matchmaking/models.py
@@ -1,1 +1,11 @@
 # Create your models here.
+from django.conf import settings
+from django.db import models
+
+from games.models import TimeControl
+
+
+class MatchmakingEntry(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_control = models.ForeignKey(TimeControl, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
